47896984bf042fc80f66f8953f64effca1c1eba8
Korapin <korapin.6945@mail.kmutt.ac.th>
2022-09-11 12:23:02 +0700

    function fillStartWord

--- fillStartWord.js
+++ fillStartWord.js
@@ -1,6 +1,20 @@
-const { template } = require('@babel/core')
+// const { template } = require('@babel/core')
 
 function fillStartWord(startWord, word) {
-  //code here
-}
-module.exports = fillStartWord
+
+    if (word == undefined || null ){
+      return undefined
+     
+    }else if(startWord==undefined || null){
+        return "startword"+word
+    }else{
+      return startWord+word
+    }
+    
+  }
+
+    console.log(fillStartWord("mom","dad"))
+    console.log(fillStartWord("Marie",null))
+    console.log(fillStartWord(undefined,"dad"))
+    console.log(fillStartWord(undefined,null))
+// module.exports = fillStartWord
