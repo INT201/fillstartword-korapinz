bb89dea98f2619c1836141f41a5e46aacd5fad50
korapinz <88102268+korapinz@users.noreply.github.com>
2022-09-11 12:42:25 +0700

    Update fillStartWord.js

--- fillStartWord.js
+++ fillStartWord.js
@@ -1,20 +1,18 @@
 const { template } = require('@babel/core')
 
-function fillStartWord(startWord, word) {
-
-    if (word == undefined || null ){
-      return undefined
-     
-    }else if(startWord==undefined || null){
-        return "startword"+word
-    }else{
-      return startWord+word
-    }
-    
+function fillStartWord(startWord,word) {
+  if(word == null){
+    return undefined
   }
+  else if(word == undefined){
+    return undefined
+  }
+ else if(startWord == 'first'){
+  return word
+ }
+else {
+  return startWord+word
+}
+}
 
-    console.log(fillStartWord("mom","dad"))
-    console.log(fillStartWord("Marie",null))
-    console.log(fillStartWord(undefined,"dad"))
-    console.log(fillStartWord(undefined,null))
 module.exports = fillStartWord
